e946d4fc7b2c0cab656366030160c53912a4c76d
Ihor-Bilan <120909956+Ihor-Bilan@users.noreply.github.com>
2024-10-02 22:40:47 +0300

    first attempt

---
+++ Class1.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    internal class TTriangle
+    {
+        protected double a, b, c;
+        public TTriangle(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new Exception("The side may be positive");
+            }; if (!IsExist(a, b, c))
+            {
+                throw new Exception("TTriangle");
+
+            }
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public bool IsExist(double a, double b, double c)
+        {
+            return (a + b > c && a + c > b && b + c > a);
+        }
+
+        public double A
+        {
+            get { return a; }
+            set
+            {
+                if (IsExist(value, b, c))
+                { a = value; }
+                else
+                {
+                    throw new Exception("Bad side");
+                }
+
+            }
+        }
+        public double B
+        {
+            get { return b; }
+            set
+            {
+                if (IsExist(a, value, c))
+                { b = value; }
+                else
+                {
+                    throw new Exception("Bad side");
+                }
+
+            }
+        }
+        public double C
+        {
+            get { return c; }
+            set
+            {
+                if (IsExist(a, b, value))
+                { c = value; }
+                else
+                {
+                    throw new Exception("Bad side");
+                }
+
+            }
+        }
+        public double Perimeter(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+
+        public double Area(double a, double b, double c)
+        {
+            return Math.Sqrt(((a + b + c)/2) * (((a + b + c)/2) - a) * (((a + b + c)/2) - b ) * (((a + b + c)/2) - c));
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the side length a");
+            Console.Write("Side a = ");
+            double side_a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the side length b");
+            Console.Write("Side b = ");
+            double side_b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the side length c");
+            Console.Write("Side c = ");
+            double side_c = double.Parse(Console.ReadLine());
+            TTriangle t1 = new TTriangle(side_a, side_b, side_c);
+            Console.WriteLine(t1.IsExist(side_a, side_b, side_c));
+            Console.WriteLine($"Perimeter is equal to {t1.Perimeter(t1.A, t1.B, t1.C)}");
+            Console.WriteLine($"Area is equal to {t1.Area(t1.A, t1.B, t1.C)}");
+        }
+    }
+}
